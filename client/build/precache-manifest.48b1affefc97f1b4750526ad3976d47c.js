2af1a818b7d8124b9950a84c6fc71581a6e7963a
unknown <mikesdatascience@yahoo.com>
2019-12-19 06:14:03 -0500

    build

---
+++ client/build/precache-manifest.48b1affefc97f1b4750526ad3976d47c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9769b3e70820971f777905b2ffcd6a29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29faa5d0ac6728992aad",
+    "url": "/static/css/main.6d7378c3.chunk.css"
+  },
+  {
+    "revision": "9f202a3b4dc48195f524",
+    "url": "/static/js/2.83a2c2d0.chunk.js"
+  },
+  {
+    "revision": "a4f8846d711431e9d8ca574f9a38ed2b",
+    "url": "/static/js/2.83a2c2d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29faa5d0ac6728992aad",
+    "url": "/static/js/main.64c5b3e1.chunk.js"
+  },
+  {
+    "revision": "f00be349455d50b09cd3",
+    "url": "/static/js/runtime-main.be67de96.js"
+  },
+  {
+    "revision": "6cd8d48386154ea66c03dc4f64878b8a",
+    "url": "/static/media/TennisnetCropped.6cd8d483.jpg"
+  }
+]);
