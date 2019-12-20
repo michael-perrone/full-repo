634595aa9209ea0d757eb0bccee2448f9681362d
unknown <mikesdatascience@yahoo.com>
2019-12-20 04:39:46 -0500

    prob wont work

---
+++ client/build/precache-manifest.e3f18cd9ca71e48e01276b1e7f3b28a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7801f575b62fdb8e34acc1c206416286",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17c502c51ff18ec440d2",
+    "url": "/static/css/main.6d7378c3.chunk.css"
+  },
+  {
+    "revision": "9f202a3b4dc48195f524",
+    "url": "/static/js/2.83a2c2d0.chunk.js"
+  },
+  {
+    "revision": "a4f8846d711431e9d8ca574f9a38ed2b",
+    "url": "/static/js/2.83a2c2d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17c502c51ff18ec440d2",
+    "url": "/static/js/main.bdbf6209.chunk.js"
+  },
+  {
+    "revision": "f00be349455d50b09cd3",
+    "url": "/static/js/runtime-main.be67de96.js"
+  },
+  {
+    "revision": "6cd8d48386154ea66c03dc4f64878b8a",
+    "url": "/static/media/TennisnetCropped.6cd8d483.jpg"
+  }
+]);
