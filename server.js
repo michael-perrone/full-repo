269c387802f43b348a2337cefe867ddedb2ff9ea
unknown <mikesdatascience@yahoo.com>
2019-12-21 09:20:34 -0500

    i think it worked

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connectedDatabase = require("./config/db");
+const path = require("path");
 
 app.use(cors());
 connectedDatabase();
