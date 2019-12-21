19f409af9a9bdf2938159984f9d4f4d1e1b4cbc6
unknown <mikesdatascience@yahoo.com>
2019-12-21 00:26:58 -0500

    not working

--- server.js
+++ server.js
@@ -47,15 +47,6 @@ app.use("/api/rebooking", require("./routes/api/rebooking.js"));
 app.use("/api/rebooked", require("./routes/api/rebooked"));
 app.use("/api/iBookings", require("./routes/api/iBookings"));
 
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "path/to/your/index.html"), function(err) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
-// dwdw
-
 app.use(express.static("./client/build"));
 
 const PORT = process.env.PORT || 8080;
