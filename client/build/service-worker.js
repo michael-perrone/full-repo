728790e54dba583d61d7c77faa344201b7181a68
unknown <mikesdatascience@yahoo.com>
2019-12-21 18:18:02 -0500

    dwd

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d895612fe3859f631e80122023772d1.js"
+  "/precache-manifest.4f25cf52fa2728d860c4e00919b6f415.js"
 );
 
 self.addEventListener('message', (event) => {
